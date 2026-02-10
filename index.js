bfe2ef699af6bbaa93aceb05e7b741bcb27e3914
sezo45 <wayaslam567@gmail.com>
2026-02-10 08:44:35 +0200

    vertion8

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('header');
-    if (window.scrollY > 50) {
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
 });
